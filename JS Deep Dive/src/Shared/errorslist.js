536d856118ff4b8481215d237531f2e0fc02751f
UnknownHex <rostpostoffice@gmail.com>
2023-03-19 12:27:21 +0300

    feat: 4task - create Task and Comment classes, add TaskCollector class

---
+++ JS Deep Dive/src/Shared/errorslist.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+const errorslist = {
+    errorTypes: {
+        validationError: 'Validation Error',
+        accessDeny: 'Access Deny',
+        notFound: 'Not found Error',
+    },
+    errorMessages: {
+        wrongUser: 'You don\'t have permissions to remove or edit this task!',
+        idNotFound: (id) => `ID "${id}" does not exist in our data!`,
+        wrongFieldMsg: (key) => `${key.toUpperCase()} field is not valid!`,
+    },
+};
